f8b496f7875a055719bcc8c20472b8c1c4e56331
Yash Hegde <yash4success@gmail.com>
2024-05-31 18:54:24 +0530

    showing the current guess on grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,7 +4,10 @@ import Row from './Row'
 function Grid({currentGuess,guessHistoryArr, guessNumber}) {
   return (
     <div>
-          {guessHistoryArr.map((guess,index) => {
+          {guessHistoryArr.map((guess, index) => {
+              if (index === guessNumber) {
+                  return (<Row key={index} currentGuess={ currentGuess} />)
+              }
               return (
                   <Row key={ index} guess= {guess} />
               )
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
-function Row({ guess }) {
+function Row({ guess,currentGuess }) {
     
     if (guess) {
         return (
-            <div className='Row Guesses'>
+            <div className='Row OtherGuesses'>
                 {guess.map((letter,index) => (
                     <div key={index} className={letter.color}>
                         {letter.key}
                     </div>
+                ))}
+            </div>
+        )
+    }
+
+    if (currentGuess) {
+        let letters = currentGuess.split('');
+        return (
+            <div className='Row currentGuess'>
+                {letters.map((letter,index) => (
+                    <div key={index} className='filled'>
+                        {letter}
+                    </div>
+                ))}
+                {[...Array(5 - letters.length)].map((_,i) => (
+                    <div key={i}></div>
                 ))}
             </div>
         )
