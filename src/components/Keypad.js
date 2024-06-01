ad40b17cdbbeff5ea6f4d492787b1591cd750d17
Yash Hegde <yash4success@gmail.com>
2024-06-01 10:23:27 +0530

    bug fixed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import wordsData from '../db/db.json';
 
 export default function Keypad({keysUsed}) {
   const [letters, setLetters] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/letters')
-      .then(res => res.json())
-      .then(json => {
-        setLetters(json)
-      })
+    const lettersArr = wordsData.letters;
+    setLetters(lettersArr);
+    
   }, [])
+
 
   return (
     <div className="keypad">
+      {console.log(letters)}
       {letters && letters.map(l => {
         const keycolor = keysUsed[l.key]
+        console.log(keycolor)
         return (
           <div key={l.key} className={keycolor}>{l.key}</div>
         )
